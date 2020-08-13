14148a1ba23e42e567394af8b7f6b122fa8df8c8
Barreto <v.barreto.and@gmail.com>
2020-08-13 07:04:25 -0300

    feat: 🎸 start user component develop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import "./styles.css";
+import { useRef } from "react";
 
 function App() {
+  const userElement = useRef(null);
+  const [mouseCursor, setMouseCursor] = useState("");
+
+  function setAutoCursor() {
+    setMouseCursor("auto-cursor");
+  }
+
+  function setGrabCursor() {
+    // Should check if wants resize
+    setMouseCursor("grab-cursor");
+  }
+
+  function setGrabbingCursor() {
+    setMouseCursor("grabbing-cursor");
+  }
+
   return (
     <div>
-      <h1>Hello World!</h1>
+      <div className="board">
+        {mouseCursor}
+        <div className={`user ${mouseCursor}`} ref={userElement}>
+          <div
+            className="head"
+            onMouseEnter={setGrabCursor}
+            onMouseDown={setGrabbingCursor}
+            onMouseUp={setGrabCursor}
+            onMouseLeave={setAutoCursor}
+          />
+          <div
+            className="body"
+            onMouseEnter={setGrabCursor}
+            onMouseDown={setGrabbingCursor}
+            onMouseUp={setGrabCursor}
+            onMouseLeave={setAutoCursor}
+          >
+            <h3>Personal Banking Customer</h3>
+            <span>[Person]</span>
+            <p>A customer of the bank, with personal bank accounts.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
